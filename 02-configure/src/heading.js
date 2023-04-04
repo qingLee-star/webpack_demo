b30e859d31be4568709be2beb8eb994278564b4c
lixiuhua <lxh_qing@163.com>
2023-04-04 15:39:42 +0800

    资源模块加载,css-loader,style-loader

--- 02-configure/src/heading.js
+++ 02-configure/src/heading.js
@@ -1,6 +1,8 @@
+import './heading.css';
 export default () => {
   const ele = document.createElement('h2');
   ele.textContent = 'hello webpack';
+  ele.classList.add('head');
   ele.addEventListener('click', () => {
     alert('hello webpack');
   });
--- 02-configure/webpack.config.js
+++ 02-configure/webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.js', // 打包入口配置
+  // entry: './src/main.css', // 打包入口配置
   output: { // 输出配置
     filename: 'bundle.js', // 输出的文件名
     path: path.join(__dirname, 'output'), // 输出的路径，要为绝对路径，不能为相对路径，所以用path模块进行拼接
+  },
+  module: { // 用于配置loader
+    rules : [
+      {
+        test: /.css$/, // 匹配文件规则
+        use: [ // 使用什么loader,这里有个注意点: 如果有多个loader需要执行,写在后面的loader会先被加载执行
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
